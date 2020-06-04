59a4add2115b026b0408d3346c5756d28a6a159e
yaboo-applied <yaboo@applied.co>
2020-06-04 23:14:15 +0900

    update src.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -54,6 +55,12 @@ int main() {
                &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
+    // start in lane 1;
+    int lane = 1;
+
+    // Have a reference velocity to target
+    double ref_vel = 49.5; // mph
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -88,6 +95,8 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
+          int prev_size = previous_path_x.size();
+
           json msgJson;
 
           vector<double> next_x_vals;
@@ -98,6 +107,122 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          std::cout << "ref_x: " << ref_x << std::endl;
+          std::cout << "ref_y: " << ref_y << std::endl;
+          std::cout << "car_yaw: " << car_yaw << std::endl;
+          std::cout << "ref_yaw: " << ref_yaw << std::endl;
+          std::cout << "prev_size: " << prev_size << std::endl;
+
+          if (prev_size < 2) {
+            //Use two points that make the path tangent to the car
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          } else {
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+            
+            //Use two points that make the path tangent to the previous path's end point
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+          // std::cout << "ptsy_size: " << ptsy.size() << std::endl;
+
+          vector<double> next_wp0 = getXY(car_s+30,(2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60,(2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90,(2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          // std::cout << "ptsy_size: " << ptsy.size() << std::endl;
+          // Global coordinate TO Reference coordinate
+          for (int i = 0; i < ptsx.size(); i++) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            ptsx[i] = (shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+            ptsy[i] = (shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+          }
+
+          // create a spline.
+          tk::spline s;
+
+          // set (x, y) points to the spline.
+          s.set_points(ptsx, ptsy);
+
+          // Start with all of the previous path points from last time.
+          for (int i = 0; i < previous_path_x.size(); i++) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          // std::cout << "previous_path_x.size(): " << previous_path_x.size() << std::endl;
+          // std::cout << "next_x_vals.size(): " << next_x_vals.size() << std::endl;
+
+          
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x * target_x) + (target_y * target_y));
+          std::cout << target_x << "," << target_y << "," << target_dist << std::endl;
+          double x_add_on = 0.0;
+          for (int i = 0; i <= 50-previous_path_x.size(); i++) {
+            double N = (target_dist/(.02*ref_vel/2.24));
+            double x_point = x_add_on + (target_x)/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+            y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+            // Note that ref_x and x_ref are different!
+            x_point += ref_x;
+            y_point += ref_y;
+
+            // std::cout << i << ": " << x_point << ", " << y_point << std::endl;
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+
+          for (int i = 0; i < next_x_vals.size(); i++) {
+            std::cout << next_x_vals[i] << ",";
+          }
+          std::cout << std::endl;
+
+          // double dist_inc = 0.5;
+          // for (int i = 0; i < 50; ++i) {
+          //   double car_next_s = car_s+((i+1)*dist_inc);
+          //   double car_next_d = 6;
+          //   vector<double> next_xy = getXY(car_next_s,car_next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          //   next_x_vals.push_back(next_xy[0]);
+          //   next_y_vals.push_back(next_xy[1]);
+          //   // next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+          //   // next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+          // }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
